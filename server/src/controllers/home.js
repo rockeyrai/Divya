d4bd033d8be220dff1df78673d825fe585177250
Rockey C.R <rockeyrai234@gmail.com>
2024-11-28 18:10:10 +0545

    fucntion to get Home Change data

--- server/src/controllers/home.js
+++ server/src/controllers/home.js
@@ -3,15 +3,12 @@ const HomeStructure = require("../models/homeModels");
 const changeHome = async (req,res)=>{
   try{
     const {homeImage,navbarColor,bodyColor,cardColor,contact,contactEmail,location } = req.body;
-
     // change home using the News model
     const home = new HomeStructure({
       homeImage,navbarColor,bodyColor,cardColor,contact,contactEmail,location 
     });
-
     // Save the new hom change to the database
     await home.save();
-
     res.status(201).json({
       message: 'Home Page Change Successfully!',
       news: home,
@@ -25,4 +22,20 @@ const changeHome = async (req,res)=>{
   }
 }
 
-module.exports = {changeHome};
+const getHomeChange = async (req,res)=>{
+  try {
+    // Retrieve all news articles from the database
+    const Change = await HomeStructure.find();
+
+    // Send the retrieved news articles as a response
+    res.status(200).json(Change);
+  } catch (error) {
+    console.error('Error fetching Change:', error);
+    res.status(500).json({
+      message: 'Error fetching Change',
+      error: error.message,
+    });
+  }
+}
+
+module.exports = {changeHome,getHomeChange};
--- server/src/routes/home.js
+++ server/src/routes/home.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const { changeHome } = require('../controllers/home');
+const { changeHome, getHomeChange } = require('../controllers/home');
 const router = express.Router();
 
-
-
 router.post("/homeui",changeHome)
+router.get('/homeui',getHomeChange)
 
 module.exports = router
