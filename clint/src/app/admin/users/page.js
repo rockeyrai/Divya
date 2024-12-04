0f261a0069cd6ea3c8dd27127aebbd07bcceb646
Rockey C.R <rockeyrai234@gmail.com>
2024-12-04 11:42:36 +0545

    makeing adming user page

---
+++ clint/src/app/admin/users/page.js
@@ -0,0 +1,64 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from '@/components/ui/button';
+
+const AdminUser = () => {
+  const [userData, setUserData] = useState([]); // Initialize as an array
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch('http://localhost:8000/user'); // Replace with your backend URL
+      if (!res.ok) throw new Error('Failed to fetch data');
+      const data = await res.json();
+      setUserData(data); // Directly set the fetched array
+    } catch (error) {
+      console.error('Error fetching user:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+        <Table>
+        <TableCaption>A list of news articles</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Phone</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {userData.map((data) => (
+            <TableRow key={data._id}>
+              <TableCell>{data.fullName}</TableCell>
+              <TableCell>{data.email}</TableCell>
+              <TableCell>{data.phoneNumber}</TableCell>
+              <TableCell>
+                <Button
+                  variant="outline"
+                  onClick={() => removeUser(data._id)}
+                >
+                  Remove
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+  
+  );
+};
+
+export default AdminUser;
